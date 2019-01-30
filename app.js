afd6530c54c9cbb193a5588939a8e0bf6de468b2
Dylan Bohlinger <d.bohlinger1@gmail.com>
2019-01-30 04:56:03 +0000

    add models and mongoose

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 var express    = require("express"),
     app        = express(),
-    bodyParser = require('body-parser');
+    ejs        = require("ejs"),
+    bodyParser = require('body-parser'),
+    mongoose   = require("mongoose");
 
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
+//DataBase
+mongoose.connect("mongodb://localhost/mealplann_v1")
+app.use(bodyParser.urlencoded({extended: true}));
+//Routes 
+
 app.get("/",function(req,res){
  res.render("landing");  
 });
+
+
+app.get()
 
 app.listen(process.env.PORT, process.env.IP,function(){
   console.log("meal plan has started") ;
---
+++ seeds.js
@@ -0,0 +1,64 @@
+var mongoose = require("mongoose");
+var Mealplan = require("./models/mealplan");
+var Comment   = require("./models/comment");
+ 
+var data = [
+    {
+        name: "Meditarian Mix", 
+        image: "https://images.unsplash.com/photo-1543352632-fea6d4f83e78?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    },
+    {
+        name: "Vegetarian Tin", 
+        image: "https://images.unsplash.com/photo-1543353071-c953d88f7033?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1225&q=80",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    },
+    {
+        name: "Kale+", 
+        image: "https://farm1.staticflickr.com/189/493046463_841a18169e.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    }
+]
+ 
+function seedDB(){
+   //Remove all campgrounds
+   Mealplan.remove({}, function(err){
+        if(err){
+            console.log(err);
+        }
+        console.log("removed mealplan!");
+        Comment.remove({}, function(err) {
+            if(err){
+                console.log(err);
+            }
+            console.log("removed comments!");
+             //add a few campgrounds
+            data.forEach(function(seed){
+                Mealplan.create(seed, function(err, campground){
+                    if(err){
+                        console.log(err)
+                    } else {
+                        console.log("added a mealplan");
+                        //create a comment
+                        Comment.create(
+                            {
+                                text: "This place is great, but I wish there was internet",
+                                author: "Homer"
+                            }, function(err, comment){
+                                if(err){
+                                    console.log(err);
+                                } else {
+                                    campground.comments.push(comment);
+                                    campground.save();
+                                    console.log("Created new comment");
+                                }
+                            });
+                    }
+                });
+            });
+        });
+    }); 
+    //add a few comments
+}
+ 
+module.exports = seedDB;
